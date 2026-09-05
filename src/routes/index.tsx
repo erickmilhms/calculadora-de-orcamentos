0ddadc77e2b65e6bb78c31b5dd5313aafa177892
Erick Milhomens <milhomenserick1@gmail.com>
2026-09-05 13:53:59 -0300

    feat: shape the budget workflow

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { BudgetCalculator } from "../components/BudgetCalculator";
+import { ScrollEffects } from "../components/ScrollEffects";
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
 function Index() {
-  return <BudgetCalculator />;
+  return (
+    <>
+      <ScrollEffects />
+      <BudgetCalculator />
+    </>
+  );
 }
