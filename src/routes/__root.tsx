3809d01a508d9b645971ac6859401e8af3f3fabb
Erick Milhomens <milhomenserick1@gmail.com>
2026-09-05 13:54:23 -0300

    style: redesign mobile quote result

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,6 +5,7 @@ import appCss from "../styles.css?url";
 import iphoneResultCss from "../iphone-result.css?url";
 import darkWorkspaceCss from "../dark-workspace.css?url";
 import iphoneStatusIconsCss from "../iphone-status-icons.css?url";
+import phoneRedesignCss from "../phone-redesign.css?url";
 
 export const Route = createRootRoute({
   head: () => ({
@@ -19,6 +20,7 @@ export const Route = createRootRoute({
       { rel: "stylesheet", href: iphoneResultCss },
       { rel: "stylesheet", href: darkWorkspaceCss },
       { rel: "stylesheet", href: iphoneStatusIconsCss },
+      { rel: "stylesheet", href: phoneRedesignCss },
       { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
     ],
   }),
